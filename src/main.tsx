b402d1d38f5d313bee25e44b0442fe66503275c5
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-02-12 02:41:07 +0900

    style: GlobaStyle 파일 생성 및 디자인 시스템 파일 생성

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import GlobalStyle from './style/GlobalStyle.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <RecoilRoot>
+    <GlobalStyle />
     <BrowserRouter>
-      {/* <GlobalStyle /> */}
       <App />
     </BrowserRouter>
   </RecoilRoot>,
