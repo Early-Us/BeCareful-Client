08f80c9310de22f5b115dd4dd9df7b60a51be4b4
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-02-12 22:30:58 +0900

    style: 순서 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
-import { ThemeProvider } from 'styled-components';
-import theme from './style/Theme';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Routes>
-        <Route path="/" />
-      </Routes>
-    </ThemeProvider>
+    <Routes>
+      <Route path="/" />
+    </Routes>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,16 @@ import App from './App.tsx';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import GlobalStyle from './style/GlobalStyle.tsx';
+import { ThemeProvider } from 'styled-components';
+import theme from './style/Theme.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <RecoilRoot>
-    <GlobalStyle />
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </RecoilRoot>,
 );
