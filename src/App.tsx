c2b7227a893d22687654a3adcd5ff4691a4a7136
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-02-17 03:50:34 +0900

    feat: 홈페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { TestPage } from '@/page/TestPage';
 import { Route, Routes } from 'react-router-dom';
 import LoginPage from './page/Login/LoginPage';
+import HomeMyworkPage from './page/Home/HomeMyworkPage';
+import HomePage from './page/Home/HomePage';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" />
+      <Route path="/" element={<HomePage />} />
       <Route path="/test" element={<TestPage />} />
       <Route path="/login" element={<LoginPage />} />
+      <Route path="/mywork" element={<HomeMyworkPage />} />
     </Routes>
   );
 }
