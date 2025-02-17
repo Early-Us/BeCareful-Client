270bb6917d41b88c2539d8e3c6ce58d558817f95
Mungjin01 <beckaljm@naver.com>
2025-02-17 16:28:08 +0900

    feat: 일자리 모달창들 구현

--- src/components/Works/WorkDetail/WorkBottomButton.tsx
+++ src/components/Works/WorkDetail/WorkBottomButton.tsx
@@ -1,16 +1,86 @@
 import { Button } from '@/components/common/Button/Button';
+import { ApplyModal } from '@/components/Works/WorkModal/ApplyModal';
+import { RefuseModal } from '@/components/Works/WorkModal/RefuseModal';
+import { RealRefuseModal } from '@/components/Works/WorkModal/RealRefuseModal';
+import { TuneConditionModal } from '@/components/Works/WorkModal/TuneConditionModal';
+import { useState } from 'react';
 import { styled } from 'styled-components';
+import { AfterTuneModal } from '@/components/Works/WorkModal/AfterTuneModal';
 
 export const WorkBottomButton = () => {
+  const [isApplyModalOpen, setIsApplyModalOpen] = useState(false);
+  const openApplyModal = () => setIsApplyModalOpen(true);
+  const closeApplyModal = () => setIsApplyModalOpen(false);
+
+  const [isRefuseModalOpen, setIsRefuseModalOpen] = useState(false);
+  const openRefuseModal = () => setIsRefuseModalOpen(true);
+  const closeRefuseModal = () => setIsRefuseModalOpen(false);
+
+  const [isRealRefuseModalOpen, setIsRealRefuseModalOpen] = useState(false);
+  const openRealRefuseModal = () => setIsRealRefuseModalOpen(true);
+  const closeRealRefuseModal = () => setIsRealRefuseModalOpen(false);
+
+  const [isTuneConditionModalOpen, setIsTuneConditionModalOpen] =
+    useState(false);
+  const openTuneConditionModal = () => setIsTuneConditionModalOpen(true);
+  const closeTuneConditionModal = () => setIsTuneConditionModalOpen(false);
+
+  const [isAfterTuneModalOpen, setIsAfterTuneModalOpen] = useState(false);
+  const openAfterTuneModal = () => setIsAfterTuneModalOpen(true);
+  const closeAfterTuneModal = () => setIsAfterTuneModalOpen(false);
+
+  const handleOpenTuneConditionModal = () => {
+    closeRefuseModal();
+    openTuneConditionModal();
+  };
+
+  const handleOpenAfterTuneModal = () => {
+    closeTuneConditionModal();
+    openAfterTuneModal();
+  };
+
+  const handleOpenRealRefuseModal = () => {
+    closeRefuseModal();
+    openRealRefuseModal();
+  };
+
   return (
     <ButtonContainer>
-      <Button variant="white" height="52px">
+      <Button variant="white" height="52px" onClick={openRefuseModal}>
         거절하기
       </Button>
 
-      <Button variant="blue" height="52px">
+      <Button variant="blue" height="52px" onClick={openApplyModal}>
         지원하기
       </Button>
+      {isApplyModalOpen && (
+        <ApplyModal width="312px" onClose={closeApplyModal} />
+      )}
+
+      {isRefuseModalOpen && (
+        <RefuseModal
+          width="312px"
+          onClose={closeRefuseModal}
+          onReject={handleOpenRealRefuseModal}
+          onTuneConditions={handleOpenTuneConditionModal}
+        />
+      )}
+
+      {isRealRefuseModalOpen && (
+        <RealRefuseModal width="312px" onClose={closeRealRefuseModal} />
+      )}
+
+      {isTuneConditionModalOpen && (
+        <TuneConditionModal
+          width="312px"
+          onClose={closeTuneConditionModal}
+          onApply={handleOpenAfterTuneModal}
+        />
+      )}
+
+      {isAfterTuneModalOpen && (
+        <AfterTuneModal width="312px" onClose={closeAfterTuneModal} />
+      )}
     </ButtonContainer>
   );
 };
---
+++ src/components/Works/WorkModal/RealRefuseModal.tsx
@@ -0,0 +1,89 @@
+import { styled } from 'styled-components';
+import { Button } from '@/components/common/Button/Button';
+
+interface RealRefuseModalProps {
+  width: string;
+  onClose: () => void;
+}
+
+export const RealRefuseModal = ({ width }: RealRefuseModalProps) => {
+  return (
+    <Overlay>
+      <ModalContent width={width}>
+        <ModalTopContainer></ModalTopContainer>
+        <ModalMiddleContainer>
+          <span>일자리가 목록에서 삭제되었어요</span>
+          <span className="highlight">
+            이선혜님께 맞는 다른 일자리를 찾아볼게요!
+            {/*TODO: 이선혜님 -> 유저 이름으로 변경*/}
+          </span>
+        </ModalMiddleContainer>
+        <ModalBottomContainer>
+          <Button variant="blue" height="52px">
+            다른 일자리 보러가기
+          </Button>
+        </ModalBottomContainer>
+      </ModalContent>
+    </Overlay>
+  );
+};
+
+const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  z-index: 1000;
+`;
+
+const ModalContent = styled.div<{ width: string }>`
+  background-color: ${({ theme }) => theme.colors.white};
+
+  border-radius: 12px;
+  width: ${({ width }) => width};
+
+  margin-bottom: 28px;
+  position: relative;
+`;
+
+const ModalTopContainer = styled.div`
+  display: flex;
+
+  height: 40px;
+
+  align-items: center;
+`;
+
+const ModalMiddleContainer = styled.div`
+  display: flex;
+  padding: 0px 20px 8px 20px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+
+  text-align: center;
+  font-size: ${({ theme }) => theme.typography.fontSize.title3};
+  font-weight: ${({ theme }) => theme.typography.fontWeight.bold};
+  color: ${({ theme }) => theme.colors.gray900};
+
+  .highlight {
+    font-size: ${({ theme }) => theme.typography.fontSize.body2};
+    font-weight: ${({ theme }) => theme.typography.fontWeight.medium};
+    color: ${({ theme }) => theme.colors.gray500};
+  }
+`;
+
+const ModalBottomContainer = styled.div`
+  display: flex;
+  padding: 16px 20px 20px 20px;
+  justify-content: center;
+  align-items: center;
+  gap: 8px;
+`;
