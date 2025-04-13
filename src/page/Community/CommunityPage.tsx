6c3c3b606802ac6c6541bd44bd68c8585fca99e0
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-04-14 02:38:06 +0900

    feat: 커뮤니티 글쓰기 페이지 추가 및 탭바 추가

--- src/page/Community/CommunityPage.tsx
+++ src/page/Community/CommunityPage.tsx
@@ -2,77 +2,83 @@ import styled from 'styled-components';
 import { ReactComponent as Search } from '@/assets/icons/Search.svg';
 import { ReactComponent as Chat } from '@/assets/icons/Chat.svg';
 import { ReactComponent as Plus } from '@/assets/icons/ButtonPlus.svg';
+import { ReactComponent as Write } from '@/assets/icons/community/Write.svg';
 import { useState } from 'react';
 import CommunityCorporation from '@/components/Community/CommunityCorporation';
 import CommunityAssociation from '@/components/Community/CommunityAssociation';
 import CommunityHome from '@/components/Community/CommunityHome';
+import CommunityWritePage from './CommunityWritePage';
+import { SocialTabBar } from '@/components/common/TabBarSocial';
 
 const CommunityPage = () => {
   const [activeTab, setActiveTab] = useState('전체');
+  const [isWriting, setIsWriting] = useState(false);
 
   return (
-    <Container>
-      <Top>
-        <div>
-          <Search />
-          <Chat />
-        </div>
-      </Top>
-
-      <Association>
-        <label className="title">장기요양협회명</label>
-        <div>
-          <label className="member">멤버 1,234</label>
-          <div className="invite">
-            <Plus />
-            <label className="invite-label">초대하기</label>
-          </div>
-        </div>
-      </Association>
-
-      <CommunityTabs>
-        {/* {['전체', '협회 공지', '공단공지', '정보 공유', '참여 신청'].map( */}
-        {['전체', '협회 공지', '공단 공지'].map((tab) => (
-          <Tab
-            key={tab}
-            active={activeTab === tab}
-            onClick={() => setActiveTab(tab)}
-          >
-            {tab}
-          </Tab>
-        ))}
-      </CommunityTabs>
-
-      {activeTab == '전체' ? (
-        <CommunityHome />
-      ) : activeTab == '협회 공지' ? (
-        <CommunityAssociation />
+    <>
+      {isWriting ? (
+        <CommunityWritePage
+          onClose={() => setIsWriting(false)}
+          boardType={activeTab}
+        />
       ) : (
-        <CommunityCorporation />
-      )}
+        <Container>
+          <Top>
+            <div>
+              <Search />
+              <Chat />
+            </div>
+          </Top>
 
-      <Button>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="18"
-          height="18"
-          viewBox="0 0 18 18"
-          fill="none"
-        >
-          <path
-            d="M16.1824 4.95954L13.0402 1.81798C12.7765 1.55435 12.4188 1.40625 12.0459 1.40625C11.6731 1.40625 11.3154 1.55435 11.0517 1.81798L2.38079 10.4882C2.24976 10.6184 2.14587 10.7734 2.07514 10.944C2.00442 11.1147 1.96825 11.2977 1.96876 11.4824V14.6247C1.96876 14.9977 2.11691 15.3553 2.38064 15.6191C2.64436 15.8828 3.00204 16.0309 3.37501 16.0309H15.1875C15.4113 16.0309 15.6259 15.9421 15.7841 15.7838C15.9424 15.6256 16.0313 15.411 16.0313 15.1872C16.0313 14.9634 15.9424 14.7488 15.7841 14.5906C15.6259 14.4323 15.4113 14.3434 15.1875 14.3434H8.78907L16.1824 6.94868C16.3131 6.81809 16.4167 6.66304 16.4874 6.49239C16.5581 6.32174 16.5945 6.13883 16.5945 5.95411C16.5945 5.76939 16.5581 5.58648 16.4874 5.41583C16.4167 5.24518 16.3131 5.09013 16.1824 4.95954ZM6.39844 14.3434H3.65626V11.6013L9.56251 5.69501L12.3047 8.4372L6.39844 14.3434ZM13.5 7.24189L10.7578 4.4997L12.0473 3.21017L14.7895 5.95236L13.5 7.24189Z"
-            fill="white"
-          />
-        </svg>
-        글쓰기
-      </Button>
-    </Container>
+          <Association>
+            <label className="title">장기요양협회명</label>
+            <div>
+              <label className="member">멤버 1,234</label>
+              <div className="invite">
+                <Plus />
+                <label className="invite-label">초대하기</label>
+              </div>
+            </div>
+          </Association>
+
+          <CommunityTabs>
+            {/* {['전체', '협회 공지', '공단공지', '정보 공유', '참여 신청'].map( */}
+            {['전체', '협회 공지', '공단 공지'].map((tab) => (
+              <Tab
+                key={tab}
+                active={activeTab === tab}
+                onClick={() => setActiveTab(tab)}
+              >
+                {tab}
+              </Tab>
+            ))}
+          </CommunityTabs>
+
+          {activeTab == '전체' ? (
+            <CommunityHome />
+          ) : activeTab == '협회 공지' ? (
+            <CommunityAssociation />
+          ) : (
+            <CommunityCorporation />
+          )}
+
+          <Button onClick={() => setIsWriting(true)}>
+            <Write />
+            글쓰기
+          </Button>
+
+          <SocialTabBar />
+        </Container>
+      )}
+    </>
   );
 };
 
 export default CommunityPage;
 
-const Container = styled.div``;
+const Container = styled.div`
+  padding-bottom: 57px;
+`;
 
 const Top = styled.div`
   height: 88px;
@@ -109,7 +115,7 @@ const Association = styled.div`
   }
 
   label {
-    background: ${({ theme }) => theme.colors.black};
+    color: ${({ theme }) => theme.colors.black};
     line-height: 140%;
   }
 
@@ -154,7 +160,7 @@ const CommunityTabs = styled.div`
 const Button = styled.button`
   z-index: 10;
   position: fixed;
-  bottom: 20px;
+  bottom: 77px;
   right: 20px;
 
   display: flex;
