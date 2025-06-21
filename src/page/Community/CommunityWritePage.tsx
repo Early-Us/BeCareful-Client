d2d131ea0202282099feaaec6c62c431a40ab3e3
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-06-22 02:42:46 +0900

    feat: 사진, 비디오, 파일 첨부 기능 및 임시저장 기능 구현, 글쓰기 작성 api 연결

--- src/page/Community/CommunityWritePage.tsx
+++ src/page/Community/CommunityWritePage.tsx
@@ -7,8 +7,13 @@ import { ReactComponent as Photo } from '@/assets/icons/community/Photo.svg';
 import { ReactComponent as File } from '@/assets/icons/community/File.svg';
 import { ReactComponent as LinkIcon } from '@/assets/icons/community/LinkIcon.svg';
 import { ReactComponent as Check } from '@/assets/icons/matching/CircleCheck.svg';
-import { useState } from 'react';
+import { ChangeEvent, useEffect, useRef, useState } from 'react';
 import BottomSheet from '@/components/Community/BottomSheet';
+import Modal from '@/components/common/Modal/Modal';
+import ModalLimit from '@/components/common/Modal/ModalLimit';
+import ModalButtons from '@/components/common/Modal/ModalButtons';
+import { postMedia, postPosting } from '@/api/community';
+import { BoardTypeMapping, MediaItem, PostRequest } from '@/types/Community';
 
 interface WritingProp {
   boardType: string;
@@ -16,6 +21,7 @@ interface WritingProp {
 }
 
 const CommunityWritePage = ({ boardType, onClose }: WritingProp) => {
+  // 게시판 bottom sheet 열림 상태
   const [isOpen, setIsOpen] = useState(false);
   const toggleSheet = () => {
     if (!isOpen) {
@@ -29,17 +35,18 @@ const CommunityWritePage = ({ boardType, onClose }: WritingProp) => {
   );
   // 시트 내에서 임시로 선택된 게시판 유형 상태
   const [tempBoard, setTempBoard] = useState(board);
-
   const handleSheetConfirm = () => {
     setBoard(tempBoard);
     setIsOpen(false);
   };
 
-  const [isMustToggle, setIsMustToggle] = useState(true);
+  // 필독 여부
+  const [isImportant, setIsImportant] = useState(true);
   const handleToggleChange = () => {
-    setIsMustToggle((prevChecked) => !prevChecked);
+    setIsImportant((prevChecked) => !prevChecked);
   };
 
+  // 게시글 제목
   const [title, setTitle] = useState('');
   const handleTitleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setTitle(e.target.value);
@@ -48,6 +55,7 @@ const CommunityWritePage = ({ boardType, onClose }: WritingProp) => {
     // textarea.style.height = `${textarea.scrollHeight}px`;
   };
 
+  // 게시글 내용
   const [content, setContent] = useState('');
   const handleContentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setContent(e.target.value);
@@ -56,25 +64,319 @@ const CommunityWritePage = ({ boardType, onClose }: WritingProp) => {
     textarea.style.height = `${textarea.scrollHeight}px`;
   };
 
-  const isActive = title.length > 0 && content.length > 0;
+  // 내용이 다 채워져 있어야 작성 버튼 클릭 가능
+  const isActive =
+    title.length > 0 && content.length > 0 && board != '게시판 선택';
 
-  const handleSaveDraft = () => {
-    // 임시 저장 로직
-    // 모달창 띄우고 localstorage에 저장
-    console.log('임시 저장:', content);
+  // limit modal
+  const [modalTitle, setModalTitle] = useState('');
+  const [modalDetail, setModalDetail] = useState('');
+  const [isLimitModalOpen, setIsLimitModalOpen] = useState(false);
+  const showLimitModal = (title: string, detail: string) => {
+    setModalTitle(title);
+    setModalDetail(detail);
+    setIsLimitModalOpen(true);
+  };
+  const handleCloseLimitModal = () => {
+    setIsLimitModalOpen(false);
+    setModalTitle('');
+    setModalDetail('');
   };
 
-  const handleSubmit = () => {
-    // 등록 로직
-    // post api 요청
-    console.log('등록:', content);
+  // 임시저장 모달
+  const [isSaveModalOpen, setIsSaveModalOpen] = useState(false);
+  // 나가기 모달
+  const [isCloseModalOpen, setIsCloseModalOpen] = useState(false);
+  // 등록 모달
+  const [isPostModalOpen, setIsPostModalOpen] = useState(false);
+
+  // 사진 첨부
+  const photoRef = useRef<HTMLInputElement>(null);
+  const handlePhotoClick = () => {
+    photoRef.current?.click();
+  };
+  const [photos, setPhotos] = useState<MediaItem[]>([]);
+  const [videos, setVideos] = useState<MediaItem[]>([]);
+  const handlePhotoChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (!files) {
+      event.target.value = '';
+      return;
+    }
+
+    const selectedFilesArray = Array.from(files);
+    const newPhotosToUpload: File[] = [];
+    const newVideosToUpload: File[] = [];
+
+    // multiple로 선택했으로 사용자가 선택한 모든 파일 검사
+    for (const file of selectedFilesArray) {
+      if (file.type.startsWith('image/')) {
+        if (photos.length + newPhotosToUpload.length > 100) {
+          showLimitModal(
+            '사진이 크기 제한을 초과해요.',
+            '사진 1장당 최대 크기는 30MB입니다.\n사진은 최대 100장까지 첨부 가능합니다.',
+          );
+          continue;
+        }
+        if (file.size > 30 * 1024 * 1024) {
+          showLimitModal(
+            '사진이 크기 제한을 초과해요.',
+            '사진 1장당 최대 크기는 30MB입니다.\n사진은 최대 100장까지 첨부 가능합니다.',
+          );
+          continue;
+        }
+        newPhotosToUpload.push(file);
+      } else if (file.type.startsWith('video/')) {
+        if (videos.length + newVideosToUpload.length > 10) {
+          showLimitModal(
+            '동영상이 크기 제한을 초과해요.',
+            '영상 1건당 최대 크기는 1GB(15분)입니다.\n영상은 최대 10건까지 첨부 가능합니다.',
+          );
+          continue;
+        }
+        if (file.size > 1 * 1024 * 1024 * 1024) {
+          showLimitModal(
+            '동영상이 크기 제한을 초과해요.',
+            '영상 1건당 최대 크기는 1GB(15분)입니다.\n영상은 최대 10건까지 첨부 가능합니다.',
+          );
+          continue;
+        }
+        newVideosToUpload.push(file);
+      }
+    }
+
+    const uploadedMedia: MediaItem[] = [];
+    try {
+      // 사진 파일
+      for (const file of newPhotosToUpload) {
+        try {
+          const uploadedFile = await postMedia(file, 'IMAGE');
+          if (uploadedFile) uploadedMedia.push(uploadedFile);
+        } catch (error) {
+          console.error('사진 파일 업로드 실패: ', error);
+        }
+      }
+      // 동영상 파일
+      for (const file of newVideosToUpload) {
+        try {
+          const uploadedFile = await postMedia(file, 'VIDEO');
+          if (uploadedFile) uploadedMedia.push(uploadedFile);
+        } catch (error) {
+          console.error('사진 파일 업로드 실패: ', error);
+        }
+      }
+    } catch (error) {
+      console.log('사진/비디오 업로드 중 오류 발생: ', error);
+    }
+
+    event.target.value = '';
+  };
+
+  // 파일 첨부
+  const fileRef = useRef<HTMLInputElement>(null);
+  const handleFileClick = () => {
+    fileRef.current?.click();
+  };
+  // 첨부된 파일 상태 관리
+  const [attachedFiles, setAttachedFiles] = useState<MediaItem[]>([]);
+  // 파일 선택 완료 시 호출될 핸들러
+  const handleFileChange = async (event: ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (!files) {
+      event.target.value = ''; // input value 초기화
+      return;
+    }
+
+    const selectedFilesArray = Array.from(files);
+    const newFilesToUpload: File[] = [];
+
+    // 현재 첨부된 파일들 총 용량 계산
+    const currentTotalSize = attachedFiles.reduce(
+      (sum, file) => sum + file.fileSize,
+      0,
+    );
+    let newFilesTotalSize = 0; // 새로 선택된 유효한 파일들의 총 용량
+
+    // 파일 개수 제한 확인
+    if (attachedFiles.length + selectedFilesArray.length > 5) {
+      showLimitModal(
+        '파일이 크기 제한을 초과해요.',
+        '파일 1건당 최대 크기는 10MB이며,\n한 게시글당 최대 크기 30MB,\n파일 개수는 5건까지 첨부 가능합니다.',
+      );
+      event.target.value = ''; // input value 초기화
+      return; // 개수 초과 시 더 이상 처리하지 않음
+    }
+
+    // 파일 유효성 검사
+    for (const file of selectedFilesArray) {
+      // 파일 용량 제한 확인 (개별 파일)
+      if (file.size > 10 * 1024 * 1024) {
+        showLimitModal(
+          '파일이 크기 제한을 초과해요.',
+          '파일 1건당 최대 크기는 10MB이며,\n한 게시글당 최대 크기 30MB,\n파일 개수는 5건까지 첨부 가능합니다.',
+        );
+        continue; // 다음 파일로 건너뛰기
+      }
+
+      // 전체 파일 용량 제한 확인 (추가될 파일 포함)
+      // 현재까지 유효한 파일들의 총 용량 + 현재 파일 용량이 전체 제한을 초과하는지 확인
+      if (currentTotalSize + newFilesTotalSize + file.size > 30 * 1024 * 1024) {
+        showLimitModal(
+          '파일이 크기 제한을 초과해요.',
+          '파일 1건당 최대 크기는 10MB이며,\n한 게시글당 최대 크기 30MB,\n파일 개수는 5건까지 첨부 가능합니다.',
+        );
+        break;
+      }
+
+      newFilesToUpload.push(file); // 유효한 파일만 추가
+      newFilesTotalSize += file.size; // 유효한 파일 용량만 합산
+    }
+
+    // 유효한 파일이 있을 경우 상태 업데이트
+    const uploadedFiles: MediaItem[] = [];
+    try {
+      for (const file of newFilesToUpload) {
+        try {
+          const uploadedFile = await postMedia(file, 'FILE');
+          if (uploadedFile) uploadedFiles.push(uploadedFile);
+        } catch (error) {
+          console.log('파일 업로드 실패: ', error);
+        }
+      }
+      // 업로드 성공한 파일들만 추가
+      setAttachedFiles((prev) => [...prev, ...uploadedFiles]);
+    } catch (error) {
+      console.log('파일 업로드 중 오류 발생: ', error);
+    }
+
+    event.target.value = '';
+  };
+
+  /*
+  // 첨부된 링크 상태 관리
+  const [links, setLinks] = useState<string[]>([]);
+  // 링크 첨부 버튼 클릭 핸들러
+  const handleLinkClick = () => {
+    const link = prompt('첨부할 링크 URL을 입력하세요:');
+    if (link) {
+      setLinks([...links, link]);
+      console.log('첨부된 링크:', link);
+    }
+  };
+  */
+  // 링크 첨부
+  const [originalUrl, setOriginalUrl] = useState('');
+  const handleLinkClick = () => {
+    const link = prompt('첨부할 링크 URL을 입력하세요:');
+    if (link) {
+      setOriginalUrl(link);
+      console.log('첨부된 링크:', link);
+    }
+  };
+
+  // 임시 저장 데이터의 localStorage 키 생성 함수
+  const getDraftStorageKey = (board: string) => `communityDraft_${board}`;
+  // localStorage에 임시 저장
+  const handleSaveDraft = () => {
+    const draftData = {
+      title,
+      content,
+      isImportant,
+      originalUrl,
+      photos,
+      videos,
+      attachedFiles,
+    };
+    const storageKey = getDraftStorageKey(board);
+    try {
+      localStorage.setItem(storageKey, JSON.stringify(draftData));
+      console.log(`${boardType} 임시 저장 완료:`, draftData);
+      setIsSaveModalOpen(!isSaveModalOpen);
+    } catch (e) {
+      console.error('임시 저장 중 오류 발생:', e);
+    }
+  };
+
+  // 임시 저장 데이터 불러오기
+  useEffect(() => {
+    const storageKey = getDraftStorageKey(board);
+    const savedDraft = localStorage.getItem(storageKey);
+
+    if (savedDraft) {
+      try {
+        const draftData = JSON.parse(savedDraft);
+        setTitle(draftData.title || '');
+        setContent(draftData.content || '');
+        setIsImportant(draftData.isImportant || false);
+        setOriginalUrl(draftData.originalUrl || '');
+        setPhotos(draftData.photos || []);
+        setVideos(draftData.videos || []);
+        setAttachedFiles(draftData.attachedFiles || []);
+      } catch (e) {
+        console.error('임시 저장 데이터 파싱 오류:', e);
+        // localStorage.removeItem(storageKey);
+      }
+    }
+  }, [boardType]);
+
+  const handleSubmit = async () => {
+    try {
+      const imageList: MediaItem[] = photos.map((file) => ({
+        fileName: file.fileName,
+        mediaUrl: file.mediaUrl,
+        fileType: 'IMAGE',
+        fileSize: file.fileSize,
+        videoDuration: 0, // 이미지에는 해당 없음
+      }));
+
+      const videoList: MediaItem[] = videos.map((file) => ({
+        fileName: file.fileName,
+        mediaUrl: file.mediaUrl,
+        fileType: 'VIDEO',
+        fileSize: file.fileSize,
+        videoDuration: file.videoDuration, // 업로드 응답에서 받은 영상 길이 사용
+      }));
+
+      const fileList: MediaItem[] = attachedFiles.map((file) => ({
+        fileName: file.fileName,
+        mediaUrl: file.mediaUrl,
+        fileType: 'FILE',
+        fileSize: file.fileSize,
+        videoDuration: 0,
+      }));
+
+      const requestBoard = BoardTypeMapping[board];
+      const postData: PostRequest = {
+        title,
+        content,
+        isImportant,
+        originalUrl,
+        imageList,
+        videoList,
+        fileList,
+      };
+
+      await postPosting(requestBoard, postData);
+
+      const storageKey = getDraftStorageKey(board);
+      localStorage.removeItem(storageKey);
+
+      console.log('게시글 작성 완료', postData);
+
+      onClose();
+    } catch (error) {
+      console.log('게시글 작성 post 실패: ', error);
+    }
   };
 
   return (
     <Container>
       <NavbarWrapper>
         <NavLeft>
-          <Close onClick={onClose} style={{ cursor: 'pointer' }} />
+          <Close
+            onClick={() => setIsCloseModalOpen(!isCloseModalOpen)}
+            style={{ cursor: 'pointer' }}
+          />
         </NavLeft>
         <NavRight isActive={isActive}>
           <button
@@ -85,7 +387,11 @@ const CommunityWritePage = ({ boardType, onClose }: WritingProp) => {
             <Store className="store-svg" />
             임시저장
           </button>
-          <button className="post" onClick={handleSubmit} disabled={!isActive}>
+          <button
+            className="post"
+            onClick={() => setIsPostModalOpen(!isPostModalOpen)}
+            disabled={!isActive}
+          >
             <Post className="post-svg" />
             등록
           </button>
@@ -116,6 +422,13 @@ const CommunityWritePage = ({ boardType, onClose }: WritingProp) => {
           <Check />
           공단 공지
         </SheetButton>
+        <SheetButton
+          active={tempBoard === '정보 공유'}
+          onClick={() => setTempBoard('정보 공유')}
+        >
+          <Check />
+          정보 공유
+        </SheetButton>
         <Buttons>
           <CancleButton onClick={() => setIsOpen(false)}>취소</CancleButton>
           <CheckButton onClick={handleSheetConfirm}>확인</CheckButton>
@@ -125,7 +438,7 @@ const CommunityWritePage = ({ boardType, onClose }: WritingProp) => {
       <MustSelect>
         <label>필독 여부</label>
         <ToggleContainer onClick={() => handleToggleChange()}>
-          <ToggleLabel checked={isMustToggle}></ToggleLabel>
+          <ToggleLabel checked={isImportant}></ToggleLabel>
         </ToggleContainer>
       </MustSelect>
 
@@ -144,11 +457,79 @@ const CommunityWritePage = ({ boardType, onClose }: WritingProp) => {
       <NavBottom>
         <Border />
         <div>
-          <Photo />
-          <File />
-          <LinkIcon />
+          <Photo onClick={handlePhotoClick} />
+          <input
+            type="file"
+            ref={photoRef}
+            style={{ display: 'none' }}
+            accept="image/*, video/*"
+            onChange={handlePhotoChange}
+            multiple // 여러 파일 선택 가능하도록 설정
+          />
+          <File onClick={handleFileClick} />
+          <input
+            type="file"
+            ref={fileRef}
+            style={{ display: 'none' }}
+            onChange={handleFileChange}
+            multiple // 여러 파일 선택 가능하도록 설정
+            accept=".pdf, .doc, .docx, .hwp" // 특정 파일 형식만 허용
+          />
+          <LinkIcon onClick={handleLinkClick} />
         </div>
       </NavBottom>
+
+      <Modal isOpen={isLimitModalOpen} onClose={handleCloseLimitModal}>
+        <ModalLimit
+          title={modalTitle}
+          detail={modalDetail}
+          onClose={handleCloseLimitModal}
+        />
+      </Modal>
+
+      <Modal
+        isOpen={isSaveModalOpen}
+        onClose={() => setIsSaveModalOpen(!isSaveModalOpen)}
+      >
+        <ModalButtons
+          title="게시물이 임시저장 되었습니다."
+          detail="작성 중인 내용은 안전하게 저장되었습니다."
+          onClose={() => setIsSaveModalOpen(!isSaveModalOpen)}
+          left="뒤로"
+          right="계속 작성하기"
+          handleRightBtnClick={() => setIsSaveModalOpen(!isSaveModalOpen)}
+        />
+      </Modal>
+
+      <Modal
+        isOpen={isCloseModalOpen}
+        onClose={() => setIsCloseModalOpen(!isCloseModalOpen)}
+      >
+        <ModalButtons
+          title="페이지에서 나가시겠습니까?"
+          detail="지금까지 작성한 내용이 모두 사라집니다."
+          onClose={() => setIsCloseModalOpen(!isCloseModalOpen)}
+          left="계속 작성하기"
+          right="나가기"
+          handleRightBtnClick={onClose}
+        />
+      </Modal>
+
+      <Modal
+        isOpen={isPostModalOpen}
+        onClose={() => setIsPostModalOpen(!isPostModalOpen)}
+      >
+        <ModalButtons
+          title="작성한 내용을 등록하시겠습니까?"
+          detail={
+            '게시물이 등록되면 즉시 게시판에 반영됩니다.\n내용을 다시 한 번 확인해주세요.'
+          }
+          onClose={() => setIsPostModalOpen(!isPostModalOpen)}
+          left="취소"
+          right="등록하기"
+          handleRightBtnClick={handleSubmit}
+        />
+      </Modal>
     </Container>
   );
 };
