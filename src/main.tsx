4a352023d9fffa59a7047f7973ef96a45c2793bf
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-06-23 04:57:54 +0900

    chore: react query 추가

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,20 @@ import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import GlobalStyle from './style/GlobalStyle.tsx';
 import { ThemeProvider } from 'styled-components';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import theme from './style/Theme.tsx';
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <RecoilRoot>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </ThemeProvider>
+    </QueryClientProvider>
   </RecoilRoot>,
 );
