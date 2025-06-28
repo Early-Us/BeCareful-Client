43a2947e135369f0049a0b4d43de314bf41005f9
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-06-29 01:37:26 +0900

    feat: 요일 format 함수 구현

--- src/constants/caregiver.ts
+++ src/constants/caregiver.ts
@@ -5,6 +5,16 @@ export const GenderMapping: { [key: string]: string } = {
   MALE: '남성',
 };
 
+const DayMapping: { [key: string]: string } = {
+  MONDAY: '월',
+  TUESDAY: '화',
+  WEDNESDAY: '수',
+  THURSDAY: '목',
+  FRIDAY: '금',
+  SATURDAY: '토',
+  SUNDAY: '일',
+};
+
 /* 요양보호사 관련 format 함수들 */
 // caretype
 export const CareTypeFormat = (caretypes: string[], length: number) => {
@@ -14,4 +24,8 @@ export const CareTypeFormat = (caretypes: string[], length: number) => {
     const count = caretypes.length - length;
     return `${caretypes.slice(0, length).join(',')} 외 ${count}개`;
   }
+};
+
+export const DayFormat = (days: string[]) => {
+  return days.map((day) => DayMapping[day]).join(', ');
 };
