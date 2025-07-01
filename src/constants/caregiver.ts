660ea9e21721c7b6062ba31e974ea9d37d26fc89
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-07-01 18:52:20 +0900

    feat: 위치, 시간 format 함수 구현

--- src/constants/caregiver.ts
+++ src/constants/caregiver.ts
@@ -1,4 +1,6 @@
 /* 요양보호사 관련 mapping 함수들 */
+import { WorkLocation } from '@/types/Caregiver/common';
+
 // 성별
 export const GenderMapping: { [key: string]: string } = {
   FEMALE: '여성',
@@ -15,6 +17,12 @@ const DayMapping: { [key: string]: string } = {
   SUNDAY: '일',
 };
 
+const TimeMapping: { [key: string]: string } = {
+  MORNING: '오전',
+  AFTERNOON: '오후',
+  EVENING: '저녁',
+};
+
 /* 요양보호사 관련 format 함수들 */
 // caretype
 export const CareTypeFormat = (caretypes: string[], length: number) => {
@@ -26,6 +34,31 @@ export const CareTypeFormat = (caretypes: string[], length: number) => {
   }
 };
 
+// workday
 export const DayFormat = (days: string[]) => {
   return days.map((day) => DayMapping[day]).join(', ');
+};
+
+// worktime
+export const TimeFormat = (times: string[]) => {
+  return times.map((time) => TimeMapping[time]).join(', ');
+};
+
+// location
+export const LocationFormat = (
+  workLocations: WorkLocation[],
+  length: number,
+) => {
+  const locations = workLocations.map((location) =>
+    location.dongEupMyeon === '전체'
+      ? `${location.siGuGun} ${location.dongEupMyeon}`
+      : `${location.dongEupMyeon}`,
+  );
+
+  if (workLocations.length <= length) {
+    return locations.join(',');
+  } else {
+    const count = locations.length - length;
+    return `${locations.slice(0, length).join(',')} 외 ${count}개`;
+  }
 };
