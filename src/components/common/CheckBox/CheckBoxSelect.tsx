89a66d402b744b61b840cf4c4bcad91af5206ed1
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-07-02 16:50:52 +0900

    fix: 체크박스 컴포넌트 오타 수정

--- src/components/common/CheckBox/CheckBoxSelect.tsx
+++ src/components/common/CheckBox/CheckBoxSelect.tsx
@@ -52,7 +52,7 @@ const Label = styled.label<{ border: boolean }>`
 
   color: ${({ theme }) => theme.colors.gray900};
   font-size: ${({ theme }) => theme.typography.fontSize.title5};
-  font-weight: ${({ theme }) => theme.typography.fontWeight.semiblod};
+  font-weight: ${({ theme }) => theme.typography.fontWeight.semibold};
 `;
 
 const Check = styled.input`
