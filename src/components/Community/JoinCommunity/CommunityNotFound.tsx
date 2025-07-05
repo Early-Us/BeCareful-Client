1e5049bd5e6f998f2ca8fe5cad0ec336774c0811
Mungjin01 <beckaljm@naver.com>
2025-07-05 11:44:56 +0900

    fix: notfound 컴포넌트 화면 중앙에 있도록 수정

--- src/components/Community/JoinCommunity/CommunityNotFound.tsx
+++ src/components/Community/JoinCommunity/CommunityNotFound.tsx
@@ -3,10 +3,12 @@ import { styled } from 'styled-components';
 
 export const CommunityNotFound = () => {
   return (
-    <CommunityNotFoundWrapper>
-      <Search />
-      <p>해당 커뮤니티가 없습니다.</p>
-    </CommunityNotFoundWrapper>
+    <CommunityNotFoundContainer>
+      <CommunityNotFoundWrapper>
+        <Search />
+        <p>해당 커뮤니티가 없습니다.</p>
+      </CommunityNotFoundWrapper>
+    </CommunityNotFoundContainer>
   );
 };
 
@@ -22,4 +24,11 @@ const CommunityNotFoundWrapper = styled.div`
   color: ${({ theme }) => theme.colors.gray700};
   font-size: ${({ theme }) => theme.typography.fontSize.title5};
   font-weight: ${({ theme }) => theme.typography.fontWeight.semibold};
+`;
+
+const CommunityNotFoundContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 56px);
 `;
