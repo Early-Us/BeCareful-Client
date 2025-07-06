c208264f67a8842d7ca6c2fd83f7783b345a23d3
Mungjin01 <beckaljm@naver.com>
2025-07-06 11:14:40 +0900

    feat: 협회 회원 타입 정의

---
+++ src/constants/associationRank.ts
@@ -0,0 +1,6 @@
+export const ASSOCIATION_RANKS = [
+  { value: 'CHAIRMAN', label: '회장' },
+  { value: 'EXECUTIVE', label: '임원' },
+  { value: 'MEMBER', label: '일반 회원' },
+  { value: 'NONE', label: '협회 비회원' },
+] as const;
--- src/types/CommunityAssociation.ts
+++ src/types/CommunityAssociation.ts
@@ -1,12 +1,21 @@
+import { ASSOCIATION_RANKS } from '@/constants/associationRank';
+
 export interface AssociationWholeList {
   associationId: number;
   associationName: string;
-  associationEstablishedYear: number;
-  associationProfileImageUrl: string;
+  associationEstablishedYear: number | null;
+  associationProfileImageUrl?: string;
   associationMemberCount: number;
 }
 
 export interface GetAssociationListResponse {
   count: number;
-  associationWholeList: AssociationWholeList[];
+  associationSimpleDtoList: AssociationWholeList[];
 }
+
+export interface JoinAssociationRequest {
+  associationId: number;
+  associationRank: AssociationRank;
+}
+
+export type AssociationRank = (typeof ASSOCIATION_RANKS)[number]['value'];
