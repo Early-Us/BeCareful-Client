e1317118d43e1a6dc2c1993684834a86337f07c2
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-07-08 18:51:45 +0900

    fix: 사회복지사 탭바 홈화면 경로 수정

--- src/constants/tabItems.socialWorker.ts
+++ src/constants/tabItems.socialWorker.ts
@@ -1,11 +1,11 @@
 import { ReactComponent as Home } from '@/assets/icons/tabbar/Home.svg';
-import { ReactComponent as Matching } from '@/assets/icons/matching/MatchingIcon.svg';
+import { ReactComponent as Matching } from '@/assets/icons/tabbar/Matching.svg';
 import { ReactComponent as Community } from '@/assets/icons/tabbar/Community.svg';
 import { ReactComponent as Mypage } from '@/assets/icons/tabbar/Mypage.svg';
 export const SOCIAL_WORKER_TAB = [
   {
     key: 'home',
-    path: '/home/social',
+    path: '/socialworker',
     label: '홈',
     Icon: Home,
   },
