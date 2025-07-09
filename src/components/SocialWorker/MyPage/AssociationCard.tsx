eccd7396a979289acdb8e094a7579f63753485bd
kwaktato <109459319+kwaktato@users.noreply.github.com>
2025-07-09 15:23:23 +0900

    feat: 사회복지사 마이페이지 컴포넌트 구현

---
+++ src/components/SocialWorker/MyPage/AssociationCard.tsx
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+
+interface AssociationCardProps {
+  association: string;
+  type: string;
+  rank: string;
+}
+
+const AssociationCard = ({ association, type, rank }: AssociationCardProps) => {
+  return (
+    <CardContainter>
+      <label className="association">{association}</label>
+
+      <InfoWrapper>
+        <div className="applyWrapper">
+          <label className="title">회원유형</label>
+          <label className="title">직급</label>
+        </div>
+        <div className="applyWrapper">
+          <label className="detail">{type}</label>
+          <label className="detail">{rank}</label>
+        </div>
+      </InfoWrapper>
+    </CardContainter>
+  );
+};
+
+export default AssociationCard;
+
+const CardContainter = styled.div`
+  display: flex;
+  padding: 20px 20px 24px 20px;
+  flex-direction: column;
+  gap: 12px;
+  border-radius: 12px;
+  border: 1px solid ${({ theme }) => theme.colors.gray50};
+  background: ${({ theme }) => theme.colors.white};
+  box-shadow: 0px 0px 8px 0px rgba(0, 0, 0, 0.08);
+
+  .association {
+    color: ${({ theme }) => theme.colors.gray900};
+    font-size: ${({ theme }) => theme.typography.fontSize.title5};
+    font-weight: ${({ theme }) => theme.typography.fontWeight.bold};
+  }
+`;
+
+const InfoWrapper = styled.div`
+  display: flex;
+  gap: 32px;
+
+  .applyWrapper {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  }
+
+  label {
+    color: ${({ theme }) => theme.colors.gray900};
+    font-size: ${({ theme }) => theme.typography.fontSize.body2};
+    font-weight: ${({ theme }) => theme.typography.fontWeight.medium};
+  }
+
+  .title {
+    color: ${({ theme }) => theme.colors.gray500};
+  }
+`;
---
+++ src/components/SocialWorker/MyPage/BelongCard.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+interface BelongCardProps {
+  title: string;
+  rank: string;
+}
+
+const BelongCard = ({ title, rank }: BelongCardProps) => {
+  return (
+    <CardContainter>
+      <label className="title">{title}</label>
+      <div className="rank">{rank}</div>
+    </CardContainter>
+  );
+};
+
+export default BelongCard;
+
+const CardContainter = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+
+  .title {
+    color: ${({ theme }) => theme.colors.gray900};
+    font-size: ${({ theme }) => theme.typography.fontSize.title5};
+    font-weight: ${({ theme }) => theme.typography.fontWeight.bold};
+  }
+
+  .rank {
+    align-self: flex-start;
+    padding: 4px 10px;
+    border-radius: 4px;
+    border: 1px solid ${({ theme }) => theme.colors.gray50};
+    background: ${({ theme }) => theme.colors.white};
+    color: ${({ theme }) => theme.colors.gray700};
+    font-size: ${({ theme }) => theme.typography.fontSize.body2};
+    font-weight: ${({ theme }) => theme.typography.fontWeight.medium};
+  }
+`;
